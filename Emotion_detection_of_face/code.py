b8d70e390e8504bb8b40064b0b25f71c7d95ceb7
mohit9905 <38405464+mohit9905@users.noreply.github.com>
2020-06-15 12:03:22 +0530

    Update code.py

--- Emotion_detection_of_face/code.py
+++ Emotion_detection_of_face/code.py
@@ -1,1 +1,101 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
 
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+%matplotlib inline
+
+
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+# GRADED FUNCTION: HappyModel
+
+def HappyModel(input_shape):
+
+    X_input=Input(input_shape)
+    
+    X=ZeroPadding2D((3,3))(X_input)
+    
+    X=BatchNormalization(axis=2)(X)
+    
+    X=Activation('relu')(X)
+    
+    X=MaxPooling2D((2,2))(X)
+    
+    X=Flatten()(X)
+    
+    X=Dense(1,activation='relu')(X)
+    
+    model=Model(inputs=X_input,outputs=X)
+   
+    
+    
+    
+
+    return model
+    
+    
+    ### START CODE HERE ### (1 line)
+happyModel = HappyModel(X_train.shape[1:])
+### END CODE HERE ###
+
+
+### START CODE HERE ### (1 line)
+opt=happyModel.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+### END CODE HERE ###
+
+
+### START CODE HERE ### (1 line)
+happyModel.fit(X_train,Y_train,epochs=40,batch_size=16)
+### END CODE HERE ###
